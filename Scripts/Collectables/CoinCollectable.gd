5a5877ff1b4e4be6698fcc2a65cc34b76d3af198
CrispyCabaret <92750729+CrispyCabaret@users.noreply.github.com>
2022-06-30 02:31:35 -0500

    Test Upload

---
+++ Scripts/Collectables/CoinCollectable.gd
@@ -0,0 +1,7 @@
+extends Collectable
+
+class_name CoinCollectable 
+
+func applyOverlap():
+	hud.increase()
+	pass
---
+++ Scripts/Collectables/PowerupCollectable.gd
@@ -0,0 +1,7 @@
+extends Collectable
+
+class_name PowerupCollectable 
+
+func applyOverlap():
+	hud.addPowerup()
+	pass
